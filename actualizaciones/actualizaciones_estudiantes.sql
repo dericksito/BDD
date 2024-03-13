d5f67d1f56d1cf3b9aaaf2b69cc81e9c2eee9a18
dericksito <derickcedeno2@icloud.com>
2024-03-13 17:21:46 -0500

    Reto-5/actualizaciones en registros

---
+++ actualizaciones/actualizaciones_estudiantes.sql
@@ -0,0 +1,42 @@
+drop table estudiantes
+create table estudiantes(
+    cedula char (10) not null,
+	nombre varchar (50) not null,
+	apellido varchar (50) not null,
+	email varchar (50) not null,
+    fecha_nacimiento date not null,
+	constraint estudiantes_pk primary key (cedula)
+	
+)
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('0850983421','Derick','Tenorio','dericktenorio@gmail.com','01/01/2001')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('0850983422','Jose','Valencia','josevalencia@gmail.com','02/02/2002')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('0850983423','Geral','Ibarra','geralibarra@gmail.com','03/03/2003')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('0850983424','Vanessa','Ortiz','vanessaortiz@gmail.com','04/04/2004')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('0850983425','Maria','Tenorio','mariatenorio@gmail.com','05/05/2005')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('0850983426','Jarel','Panezo','jarelpanezo@gmail.com','06/06/2006')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('0850983427','Nadine','Cedeno','nadinecedeno@gmail.com','07/07/2007')
+
+insert into estudiantes(cedula,nombre,apellido,email,fecha_nacimiento)
+values('085098348','Oswaldo','Tenorio','oswaldotenorio@gmail.com','08/08/2008')
+
+
+select nombre,cedula from estudiantes
+select nombre from estudiantes where cedula like '17%'
+select nombre,apellido from estudiantes where nombre like 'A%'
+
+update estudiantes set apellido='Hernandez'
+where cedula like '17%'
---
+++ actualizaciones/actualizaciones_videojuegos.sql
@@ -0,0 +1,45 @@
+ drop table videojuegos
+ create table videojuegos(
+    codigo int  not null,
+	nombre varchar (100) not null,
+	descripcion varchar (300),
+	valoracion int not null,
+	constraint videojuegos_pk primary key (codigo)
+	
+)
+
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values(1,'FREE FIRE','batle royale de armas',5)
+
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values(2,'pug mobile','batle royale de armas',4)
+
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values(3,'letal company','supervivencia',4)
+
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values(4,'Fornite','batle royale de armas',5)
+
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values(9,'Plato','Juegos de mesa',20)
+
+insert into videojuegos(codigo,nombre,descripcion,valoracion)
+values(5,'FIFA','deportes',5)
+
+insert into videojuegos(codigo,nombre,valoracion)
+values(6,'EFOTBALL',3)
+
+insert into videojuegos(codigo,nombre,valoracion)
+values(7,'fall guys',4)
+
+insert into videojuegos(codigo,nombre,valoracion)
+values(8,'stumbles guys',2)
+
+
+select * from videojuegos where nombre like 'C%'
+select * from videojuegos where valoracion between 9 and 10
+select * from videojuegos where descripcion is null
+
+
+update videojuegos set descripcion='Mejor Puntuado'
+where valoracion > 9
